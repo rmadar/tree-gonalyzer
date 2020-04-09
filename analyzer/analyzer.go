f6dd9214f2ecb1f48b02db9a7d428d925ecb469b
rmadar <romain.madar@gmail.com>
2020-04-09 23:19:58 +0200

    First commit for a working example

---
+++ analyzer/analyzer.go
@@ -0,0 +1,19 @@
+package analyzer
+
+
+import (
+
+	"go-hep.org/x/hep/hbook"
+	
+	"tree-gonalyzer/sample"
+	"tree-gonalyzer/variable"
+)
+
+// Starting to implement the Analyzer object
+type Analyzer struct {
+	Samples []sample.Spl      // sample on which to run
+	SplGroup string           // specify how to group sample together
+	Variables []*variable.Var // variables to plot
+	Selections []string       // implement a type selection ?
+	Histos []*hbook.H1D       // multiple map histo[var, sample, cut, syst]
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/rmadar/hplot-style v0.0.0-20200409093828-186ad95d84d6
-	go-hep.org/x/hep v0.25.1-0.20200408105008-04dc20661541
-	gonum.org/v1/plot v0.7.1-0.20200409074754-e32acf92b684
+	go-hep.org/x/hep v0.25.1-0.20200409111103-d62708a8fdf5
+	gonum.org/v1/plot v0.7.1-0.20200409110503-eddda93a2277
 )
