1d2ca460f12825e8f61d2a16c3fe93faf549dee2
rmadar <romain.madar@gmail.com>
2020-04-10 14:42:48 +0200

    Plotting style in the variable type

--- variable/variable.go
+++ variable/variable.go
@@ -3,13 +3,14 @@ package variable
 
 import (
 	"fmt"
+	
+	"go-hep.org/x/hep/hplot"
 )
 
 type Var struct {
 	OutputName string
 	TreeName string
 	Value interface{}
-	Type string
 	Nbins int
 	Xmin, Xmax float64
 	PlotTitle string
@@ -35,3 +36,49 @@ func (v Var) GetValue() float64 {
         }
 }
 
+
+func (v Var) SetPlotStyle(p *hplot.Plot) {
+
+	// Plot labels
+	if &v.PlotTitle != nil {
+		p.Title.Text = v.PlotTitle
+	}
+	if &v.XLabel != nil {
+		p.X.Label.Text = v.XLabel
+	}
+	if &v.YLabel != nil {
+		p.Y.Label.Text = v.YLabel
+	}
+
+	// Axis ranges
+	if &v.RangeXmin != nil {
+		p.X.Min = v.RangeXmin
+	}
+	if &v.RangeXmax != nil {
+		p.X.Max = v.RangeXmax
+	}
+	if &v.RangeYmin != nil {
+		p.X.Min = v.RangeXmin
+	}
+	if &v.RangeYmax != nil {
+		p.X.Max = v.RangeXmax
+	}
+	
+	// Legend setup
+	if &v.LegPosTop != nil {
+		p.Legend.Top = v.LegPosTop
+		if p.Legend.Top {
+			p.Legend.YOffs = -5
+		} else {
+			p.Legend.YOffs =  5
+		}
+	}
+	if &p.Legend.Left != nil {
+		p.Legend.Left = v.LegPosLeft
+		if p.Legend.Left {
+			p.Legend.XOffs = 5
+		} else {
+			p.Legend.XOffs = -5
+		}
+	}
+}
