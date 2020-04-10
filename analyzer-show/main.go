6d4db5c3a212087db253b61c9a8d85ba34a50594
rmadar <romain.madar@gmail.com>
2020-04-10 14:44:17 +0200

    Update the example with last feature

--- analyzer-show/main.go
+++ analyzer-show/main.go
@@ -2,44 +2,73 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"math"
+	
 	"image/color"
 	
-	"gonum.org/v1/plot/vg"
-	
-	"go-hep.org/x/hep/groot"
-	"go-hep.org/x/hep/groot/rtree"
-	
-	"go-hep.org/x/hep/hbook"
-	"go-hep.org/x/hep/hplot"
-	"go-hep.org/x/hep/hplot/htex"
-	
-	"github.com/rmadar/hplot-style/style"
-
+	"tree-gonalyzer/analyzer"
 	"tree-gonalyzer/sample"
 	"tree-gonalyzer/variable"
 )
 
+// Run the analyzer
+func main(){
 
+	// Create analyzer object
+	ana := analyzer.Ana{
+		
+		Samples: []sample.Spl{
+			spl1,
+			spl2,
+			spl3,
+		},
+		
+		Variables: []*variable.Var{
+			var_dphi,
+			/*var_Ckk,
+			var_Crr,
+			var_Cnn,
+			var_pt_lep,
+			var_eta_lep,
+			var_pt_b,
+			var_eta_b,
+			var_pt_t,
+			var_eta_t,*/
+			var_pt_vsum,
+			/*var_m_tt,
+			var_pt_tt,*/
+		},
+	}
+	
+	// Create histograms via an event loop
+	err := ana.MakeHistos()
+	if err != nil {
+		panic(err)
+	}
+	
+	// Plot them on the same canvas
+	err = ana.PlotHistos()
+	if err != nil {
+		panic(err)
+	}
 
-// All samples
-var all_samples = []sample.Spl {
+}
 
-	sample.Spl{
+// Define all samples and variables of the analysis
+var (
+	spl1 = sample.Spl{
 		FileName: "../testdata/ttbar_MadSpinOn.root",
 		TreeName: "truth",
-		LegLabel: `$t\bar{t}$ w/ spin`,
+		LegLabel: `MadGraph Prediction`,
 		LineColor: color.NRGBA{R:  50, G:  20, B: 150, A: 20},
 		FillColor: color.NRGBA{R:  50, G:  20, B: 150, A: 20},
 		LineWidth: 0,
 		CircleMarkers: false,
 		CircleSize: 3,
 		WithYErrBars: false,
-	},
+	}
 	
-	sample.Spl{
+	spl2 = sample.Spl{
 		FileName: "../testdata/ttbar_MadSpinOff.root",
 		TreeName: "truth",
 		LegLabel: `$t\bar{t}$ MG w/o spin`,
@@ -48,9 +77,9 @@ var all_samples = []sample.Spl {
 		CircleMarkers: false,
 		CircleSize: 1.5,
 		WithYErrBars: false,
-	},
+	}
 
-	sample.Spl{
+	spl3 = sample.Spl{
 		FileName: "../testdata/ttbar_ME.root",
 		TreeName: "truth",
 		LegLabel: `$t\bar{t}$ full ME`,
@@ -59,16 +88,12 @@ var all_samples = []sample.Spl {
 		CircleMarkers: false,
 		CircleSize: 1.5,
 		WithYErrBars: false,
-	},		
-}
-
-// All variable.variables
-var (
+	}		
+	
 	var_dphi= &variable.Var{
 		OutputName: "truth_dphi_ll.tex",
 		TreeName: "truth_dphi_ll",
 		Value: new(float64),
-		Type: "float64",
 		Nbins: 15,
 		Xmin: 0, 
 		Xmax: math.Pi,
@@ -84,7 +109,6 @@ var (
 		OutputName: "truth_Ckk.tex",
 		TreeName: "truth_Ckk",
 		Value: new(float64),
-		Type: "float64",
 		Nbins: 25,
 		Xmin: -1, 
 		Xmax:  1,
@@ -101,7 +125,6 @@ var (
 		OutputName: "truth_Crr.tex",
 		TreeName: "truth_Crr",
 		Value: new(float64),
-		Type: "float64",
 		Nbins: 25,
 		Xmin: -1, 
 		Xmax:  1,
@@ -118,7 +141,6 @@ var (
 		OutputName: "truth_Cnn.tex",
 		TreeName: "truth_Cnn",
 		Value: new(float64),
-		Type: "float64",
 		Nbins: 25,
 		Xmin: -1, 
 		Xmax:  1,
@@ -135,7 +157,6 @@ var (
 		OutputName: "pt_lep.tex",
 		TreeName: "l_pt",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 0, 
 		Xmax: 500,
@@ -151,7 +172,6 @@ var (
 		OutputName: "eta_lep.tex",
 		TreeName: "l_eta",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: -5, 
 		Xmax: 5,
@@ -168,7 +188,6 @@ var (
 		OutputName: "pt_b.tex",
 		TreeName: "b_pt",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 0, 
 		Xmax: 500,
@@ -183,7 +202,6 @@ var (
 		OutputName: "eta_b.tex",
 		TreeName: "b_eta",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: -5, 
 		Xmax: 5,
@@ -199,7 +217,6 @@ var (
 		OutputName: "pt_vsum.tex",
 		TreeName: "vsum_pt",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 0, 
 		Xmax: 250,
@@ -214,7 +231,6 @@ var (
 		OutputName: "pt_t.tex",
 		TreeName: "t_pt",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 0, 
 		Xmax: 500,
@@ -229,7 +245,6 @@ var (
 		OutputName: "eta_t.tex",
 		TreeName: "t_eta",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: -5, 
 		Xmax: 5,
@@ -245,7 +260,6 @@ var (
 		OutputName: "m_tt.tex",
 		TreeName: "ttbar_m",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 300, 
 		Xmax: 1000,
@@ -261,7 +275,6 @@ var (
 		OutputName: "pt_tt.tex",
 		TreeName: "ttbar_pt",
 		Value: new(float32),
-		Type: "float32",
 		Nbins: 25,
 		Xmin: 0, 
 		Xmax: 150,
@@ -272,153 +285,3 @@ var (
 		LegPosLeft: false,
 	} 
 )
-
-var all_variables = []*variable.Var {
-	var_dphi,
-	var_Ckk,
-	var_Crr,
-	var_Cnn,
-	var_pt_lep,
-	var_eta_lep,
-	var_pt_b,
-	var_eta_b,
-	var_pt_t,
-	var_eta_t,
-	var_pt_vsum,
-	var_m_tt,
-	var_pt_tt,
-}
-
-func main(){
-
-	// Get histos
-	histos_Nvars_Nsamples, err := makeHistos()
-	if err != nil {
-		panic(err)
-	}
-
-	// Plot them on the same canvas
-	plotHistos(histos_Nvars_Nsamples)
-}
-
-
-func makeHistos() ([][]*hbook.H1D, error) {
-
-	// Build a 2D-slices of histogram [nall_variables][nsamples]
-	s2d_histos := make([][]*hbook.H1D, len(all_variables))
-	for i := range s2d_histos{
-		s2d_histos[i] = make([]*hbook.H1D, len(all_samples))
-	}
-	
-	// Loop over the samples
-	for is, s := range all_samples {
-		
-		// Create H1D objects for each variables, for each samples
-		for iv, v := range all_variables {
-			s2d_histos[iv][is] = hbook.NewH1D(v.Nbins, v.Xmin, v.Xmax)
-		}
-
-		func(j int) { // Anonymous function to avoid memory leak due to 'defer'
-		
-			// Get the file and tree
-			f, tree := getTreeFromFile(s.FileName, s.TreeName)
-			defer f.Close()
-			
-			// Prepare the variables to read
-			rvars := []rtree.ReadVar{}
-			for _, v := range all_variables {
-				rvars = append(rvars, rtree.ReadVar{Name: v.TreeName, Value: v.Value})
-			}
-			
-			// Get the tree reader
-			r, err := rtree.NewReader(tree, rvars)
-			if err != nil {
-				log.Fatalf("could not create tree reader: %w", err)
-			}
-			defer r.Close()
-			
-			// Read the tree
-			err = r.Read(func(ctx rtree.RCtx) error {
-				
-				for iv, v := range all_variables {
-					s2d_histos[iv][is].Fill(v.GetValue(), 1)
-				}
-				
-				return nil
-			})
-		}(is)
-			
-	}	
-
-	return s2d_histos, nil
-}
-
-
-
-func plotHistos(histos [][]*hbook.H1D) {
-
-	// Loop over variables and get histo for all samples
-	for iv, hsamples := range histos { 
-		
-		// Create a new styled plot
-		p := hplot.New()
-		p.Latex = htex.DefaultHandler
-		style.ApplyToPlot(p)
-
-		// Plot labels
-		if &all_variables[iv].PlotTitle != nil {
-			p.Title.Text = all_variables[iv].PlotTitle
-		}
-		p.X.Label.Text = all_variables[iv].XLabel
-		p.Y.Label.Text = all_variables[iv].YLabel
-		p.X.Min = all_variables[iv].RangeXmin
-		p.X.Max = all_variables[iv].RangeXmax
-		p.Y.Min = all_variables[iv].RangeYmin
-		p.Y.Max = all_variables[iv].RangeYmax
-		
-		// Loop over samples and turn hook.H1D into styled plottable histo
-		for is, h := range hsamples {
-			h.Scale(1.0/h.Integral())
-			hist := all_samples[is].CreateHisto(h)
-			p.Legend.Add(all_samples[is].LegLabel, hist)
-			p.Add(hist)
-		}
-
-		// Legend
-		p.Legend.Top = all_variables[iv].LegPosTop
-		p.Legend.Left = all_variables[iv].LegPosLeft
-		p.Legend.YOffs = -5
-		if p.Legend.Left {
-			p.Legend.XOffs = 5
-		} else {
-			p.Legend.XOffs = -5
-		}
-		p.Legend.TextStyle.Font.Size = 12
-		
-		// Save the plot
-		if err := p.Save(5.5*vg.Inch, 4*vg.Inch, "results/"+all_variables[iv].OutputName); err != nil {
-			log.Fatalf("error saving plot: %v\n", err)
-		}
-	}
-}
-
-
-// Helper to get a tree from a file
-func getTreeFromFile(filename, treename string) (*groot.File, rtree.Tree) {
-
-	// Get the file
-	f, err := groot.Open(filename)
-	if err != nil {
-		err := fmt.Sprintf("could not open ROOT file %q: %w", filename, err)
-		panic(err)
-	}
-	
-	// Get the tree
-	obj, err := f.Get(treename)
-	if err != nil {
-		err := fmt.Sprintf("could not retrieve object: %w", err)
-		panic(err)
-	}		
-
-	return f, obj.(rtree.Tree)
-}
