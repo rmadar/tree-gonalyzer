8a145a6376e05c4c14356974545af7e276b26649
rmadar <romain.madar@gmail.com>
2020-05-21 23:35:51 +0200

    Update documentation

--- ana/example_sample_test.go
+++ ana/example_sample_test.go
@@ -1,5 +1,14 @@
 package ana_test
 
+// Default samples for data, background and signal. 
 func ExampleNewSample() {
 
+	// Data sample
+	sData := ana.NewSample("DATA", "data", `pp data 2020`, "myfile.root", "mytree")
+
+	// Background sample, say QCD pp->ttbar production
+	sBkg := ana.NewSample("ttbar", "bkg", `$pp \to t\bar{t}`, "myfile.root", "mytree")
+
+	// Signal sample, say  pp->H->ttbar production
+	sSig := ana.NewSample("Htt", "sig", `$pp \to H \to t\bar{t}`, "myfile.root", "mytree")
 }
