77d751b1f9a352038300a7a5d6750efa68d78a41
rmadar <romain.madar@gmail.com>
2020-05-21 23:51:16 +0200

    Update example

--- ana/example_sample_test.go
+++ ana/example_sample_test.go
@@ -14,20 +14,20 @@ func ExampleNewSample() {
 
 func ExampleSample_withWeight() {
 	// Define a weight from a single branch
-	s := ana.NewSample("Htt", "sig", `Higgs prod.`, "myfile.root", "mytree",
+	s1 := ana.NewSample("Htt", "sig", `Higgs prod.`, "myfile.root", "mytree",
 		ana.WithWeight(ana.NewTreeFruncF64("evt_weight")),
 	)
 
-	// Define a  weight computed from several branches 
-	wComputed :=  ana.TreeFunc{
+	// Define a weight computed from several branches 
+	w := ana.TreeFunc{
 		VarsName: []string{"w1", "w2", "w3"},
-		Fct: func (w1, w2, w3 float64) float64 {return w1*w2*w3},
+		Fct: func (w1, w2, w3 float64) float64 {
+			return w1*w2*w3
+		},
 	}
-	
 	s2 := ana.NewSample("Htt", "sig", `Higgs prod.`, "myfile.root", "mytree",
 		ana.WithWeight(w),
 	)
-
 }
 
 func ExampleSample_withCut() {
