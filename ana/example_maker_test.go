1c75301e998085d3bf66bb4b14e7fce5c051992e
rmadar <romain.madar@gmail.com>
2020-05-22 23:20:35 +0200

    Starting to write example for analysis maker

--- ana/example_maker_test.go
+++ ana/example_maker_test.go
@@ -1,13 +1,17 @@
 package ana_test
 
+import (
+	"github.com/rmadar/tree-gonalyzer/ana"
+)
+
 // Creation of the default analysis maker type
 func ExampleMaker_default() {
 	// Define Samples
 	samples := []*ana.Sample{
-		ana.NewSample("data", "data", `Data`, "data.root", "mytree")
-		ana.NewSample("bkg1", "bkg", `Proc 1`, "proc1.root", "mytree")
-		ana.NewSample("bkg2", "bkg", `Proc 2`, "proc2.root", "mytree")
-		ana.NewSample("bkg3", "bkg", `Proc 3`, "proc3.root", "mytree")
+		ana.NewSample("data", "data", `Data`, "data.root", "mytree"),
+		ana.NewSample("bkg1", "bkg", `Proc 1`, "proc1.root", "mytree"),
+		ana.NewSample("bkg2", "bkg", `Proc 2`, "proc2.root", "mytree"),
+		ana.NewSample("bkg3", "bkg", `Proc 3`, "proc3.root", "mytree"),
 	}
 	
 	// Variables
