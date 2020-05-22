5414a3c4e5a9b55cabe1c3a8c060ebfb44a6e927
rmadar <romain.madar@gmail.com>
2020-05-22 23:17:10 +0200

    Starting to write example for analysis maker

--- ana/example_maker_test.go
+++ ana/example_maker_test.go
@@ -2,7 +2,27 @@ package ana_test
 
 // Creation of the default analysis maker type
 func ExampleMaker_default() {
+	// Define Samples
+	samples := []*ana.Sample{
+		ana.NewSample("data", "data", `Data 2020`, "../testdata/ttbar_MadSpinOff.root", "truth"),
+		ana.NewSample("bkg1", "bkg", `Proc 1`, "../testdata/ttbar_MadSpinOn_1.root", "truth"),
+		ana.NewSample("bkg2", "bkg", `Proc 2`, "../testdata/ttbar_MadSpinOn_2.root", "truth"),
+		ana.NewSample("bkg3", "bkg", `Proc 3`, "../testdata/ttbar_MadSpinOn_1.root", "truth"),
+	}
+	
+	// Variables
+	variables := []*ana.Variable{
+		ana.NewVariable("truth_dphi_ll", "truth_dphi_ll", new(float64), 15, 0, math.Pi),
+		ana.NewVariable("m_tt", "ttbar_m", new(float32), 25, 300, 1000),
+	}
 
+	// Create analyzer object
+	analyzer := ana.New(samples, variables)
+	
+	// Run the analyzer and produce all plots
+	if err := analyzer.Run(); err != nil {
+		panic(err)
+	}
 }
 
 // Creation of the Stack analysis maker type
