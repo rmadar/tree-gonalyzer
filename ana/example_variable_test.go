7f832b296f9b75672cd137bd274dcc376cb14731
rmadar <romain.madar@gmail.com>
2020-05-26 00:06:16 +0200

    Update doc

--- ana/example_variable_test.go
+++ ana/example_variable_test.go
@@ -1,12 +1,12 @@
 package ana_test
 
-func ExampleVariable_default() {
+func ExampleNewVariable() {
 	// Variable 'name' corresponding to the branch 'branchF64'
 	// to be histogrammed with 100 bins between 0 and 100.
 	NewVariable("name", "branchF64", new(float64), 100, 0, 100)
 }
 
-func ExampleVariable_fromTreeFunc() {
+func ExampleNewVariableFromTreeFunc() {
 	// Define the TreeFunc
 	fct := ana.TreeFunc{
 		VarsName: []string{"br1", "br2", "br3"},
