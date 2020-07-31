6832513dbeeaddb1a6a492f5d37de19ec3fc6d91
rmadar <romain.madar@gmail.com>
2020-07-31 23:09:08 +0200

    First commit of the cutflow subpackage

---
+++ cutflow/cutflow.go
@@ -0,0 +1,63 @@
+// Doc for cutflow package
+package cutflow
+
+import (
+	"fmt"
+	
+	"go-hep.org/x/hep/groot/rtree"
+)
+
+// Event model interface
+type Event interface {
+	rvars() []rtree.ReadVar
+	weight() float64
+}
+
+// Event yields type with both raw and
+// weighted yields, and a name for a cut stage.
+type Yields struct {
+	Name   string  // Name of the cut stage.
+	Nraw   float64 // Raw yields.
+	Nwgt   float64 // Weighted yields, weight is defined by Event.weight()
+}
+
+// CutFlow is a slice of Yields, once per cut.
+type CutFlow []Yields
+
+// Cut contains the needed information
+type Cut struct {
+	Name string
+	Pass func(e *Event) bool
+}
+
+// Serie of several cuts.
+type CutSequence []Cut
+
+// NewCutSequence create a cut sequence from
+// a list of cuts.
+func NewCutSeq(cuts ...Cut) CutSequence {
+	cs := make([]Cut, len(cuts))
+	for i, c := range cuts {
+		cs[i] = c
+	}
+	return cs
+}
+
+// From() creates a CutFlow object corresponding
+// to a given cut sequence.
+func From(cuts CutSequence) CutFlow {
+	cf := make([]Yields, len(cuts))
+	for i, cut := range cuts {
+		cf[i].Name = cut.Name
+	}
+	return cf
+}
+
+// Cutflow string formater
+func (cf CutFlow) String() string {
+	var str string
+	for _, y := range cf {
+		str += fmt.Sprintf("%v %v %v %v\n", y.Name, y.Nraw, y.Nwgt)
+	}
+	return str
+}
---
+++ cutflow/maker.go
@@ -0,0 +1,96 @@
+package cutflow
+
+import (
+	"log"
+	
+	"go-hep.org/x/hep/groot"
+	"go-hep.org/x/hep/groot/rtree"
+)
+
+type Maker struct {
+
+	// Event model implementing the Event interface.
+	// It requires two functions, namely Event.rvars()
+	// and Event.weight(), to be defined.
+	Event *Event
+
+	// Selection applied before checking individual cuts
+	// of the cut sequence.
+	Preselection func(e *Event) bool
+
+	// Cut sequence defining each stage of the cut flow.
+	Cuts CutSequence
+
+	// List of the name of files to be analyzed.
+	FilesName []string
+
+	// Name of the TTree to be analyzed.
+	TreeName string
+}
+
+func (ana *Maker) CutFlow() CutFlow {
+
+	// Full rtree
+	var tree rtree.Tree
+
+	// Loop over files to get the full tree
+	for iFile, fName := range ana.FilesName {
+
+		// Open the file
+		f, err := groot.Open(fName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		// Get the tree
+		obj, err := f.Get(ana.TreeName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		t := obj.(rtree.Tree)
+
+		// Chain to the full tree
+		switch iFile {
+		case 0:
+			tree = t
+		default:
+			tree = rtree.Chain(tree, t) 
+		}
+	}
+	
+	// Event type
+	var e Event
+
+	// Tree reader
+        r, err := rtree.NewReader(tree, e.rvars())  
+        if err != nil {                                               
+                log.Fatalf("could not create tree reader: %+v", err)  
+        }                                                             
+        defer r.Close()
+	
+	// Cut sequence and associated cutflow
+	cutSeq  := NewCutSeq(ana.Cuts...)
+	cutFlow := From(cutSeq)
+
+	// Loop over events
+        err = r.Read(func(ctx rtree.RCtx) error {  
+
+		// Apply preselection
+		if !ana.Preselection(&e) {
+			return nil
+		}
+		
+		// Loop over the cuts
+		for ic, cut := range ana.Cuts {
+			if cut.Pass(&e) {
+				cutFlow[ic].Nraw += 1
+				cutFlow[ic].Nwgt += e.weight()
+			}
+		}
+		
+		return nil
+	})
+
+	// return the result
+	return cutFlow
+}
